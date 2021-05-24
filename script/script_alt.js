dc07235b59d129b289c68d8d803d6b849c056874
michawempe <83657220+michawempe@users.noreply.github.com>
2021-05-24 16:46:37 +0200

    Update_210524

--- script/script_alt.js
+++ script/script_alt.js
@@ -1,5 +1,3 @@
-// Load Video
-
 var video0 = document.getElementById("v0");
 var video1 = document.getElementById("v1");
 var video2 = document.getElementById("v2");
@@ -12,41 +10,33 @@ video2.src = "https://www.dropbox.com/s/zekcgjjql5a71qr/Plakat_1080_210517.mp4?r
 video3.src = "https://www.dropbox.com/s/2wnhupypihkpsq1/Bierdeckel_1080_210517.mp4?raw=1";
 video4.src = "https://www.dropbox.com/s/evb44wf3pbouk8q/Generativ_1080_210517.mp4?raw=1";
 
-// Playing Video on Scroll
-
-var frameNumber = 0;
-var playbackConst = [850, 970, 1700, 1500, 1470];
-
-value = 0;
-
 window.onscroll = function() {
-  if ((window.pageYOffset) <= 4000) {
-    value = 0;
-  } else if ((window.pageYOffset) <= 8000) {
-    value = 1;
-  } else if ((window.pageYOffset) <= 12000) {
-    value = 2;
-  } else if ((window.pageYOffset) <= 16000) {
-    value = 3;
+  if ((window.pageYOffset) <= (window.innerHeight * 2)) {
+    video0.play();
+    video1.pause();
+  } else if ((window.pageYOffset) <= (window.innerHeight * 4)) {
+    video0.pause();
+    video1.play();
+    video2.pause();
+  } else if ((window.pageYOffset) <= (window.innerHeight * 6)) {
+    video1.pause();
+    video2.play();
+    video3.pause();
+  } else if ((window.pageYOffset) <= (window.innerHeight * 8)) {
+    video2.pause();
+    video3.play();
+    video4.pause();
   } else {
-    value = 4;
+    video3.pause();
+    video4.play();
   }
-
-  video = document.getElementById('v' + value);
-
-  function scrollPlay(){
-    var frameNumber  = (window.pageYOffset - (value * 4000))/playbackConst[value];
-    video.currentTime  = frameNumber;
-    window.requestAnimationFrame(scrollPlay);
-  }
-  window.requestAnimationFrame(scrollPlay);
 };
 
 
 // Navigation Box Indicator
 
 document.onscroll = function( e ) {
-  var animationBox = ((window.pageYOffset / 4000) * (window.innerHeight / 5) - 8);
+  var animationBox = ((window.pageYOffset / (window.innerHeight * 2)) * (window.innerHeight / 5) - 8);
   document.getElementById("navigationBox").style.top = animationBox + "px";
 };
 
